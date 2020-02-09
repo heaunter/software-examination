b184e14b7934153a91afe976b59a255d7bcb0cc6
mengzg <mengzhg@126.com>
2020-02-09 22:07:38 +0800

    IsPowerOfTwo

---
+++ src/main/java/IsPowerOfTwo.java
@@ -0,0 +1,14 @@
+public class IsPowerOfTwo {
+
+    public static void main(String[] args) {
+        IsPowerOfTwo isPowerOfTwo = new IsPowerOfTwo();
+        int input = 20;
+        boolean ans = isPowerOfTwo.isPowerOfTwo(input);
+        System.out.println(ans);
+
+    }
+
+    public boolean isPowerOfTwo(int n) {
+        return Boolean.TRUE;
+    }
+}
